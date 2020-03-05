93eb67743b4997f07ac6ef9a18aa91f833f2c586
bookstore <booookstore@icloud.com>
2020-03-05 20:01:23 +0900

    step4-7. 関数の抽出

--- src/main/java/demo/refactoring/Statement.java
+++ src/main/java/demo/refactoring/Statement.java
@@ -7,16 +7,23 @@ public class Statement {
     public static String print(Invoice invoice, Map<String, Play> plays) throws Exception {
         StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer() + "\n");
 
-        int totalAmount = 0;
+        int totalAmount = totalAmount(invoice, plays);
         for (Performance perf : invoice.getPerformances()) {
             // 注文の内訳を出力
             result.append("  ").append(playFor(plays, perf).getName()).append(": ").append(amountFor(perf, playFor(plays, perf)) / 100).append(" (").append(perf.getAudience()).append(" seats)\n");
-            totalAmount += amountFor(perf, playFor(plays, perf));
         }
 
         result.append("Amount owed is ").append(totalAmount / 100).append("\n");
         result.append("You earned ").append(totalVolumeCredits(invoice, plays)).append(" credits");
         return result.toString();
+    }
+
+    private static int totalAmount(Invoice invoice, Map<String, Play> plays) throws Exception {
+        int totalAmount = 0;
+        for (Performance perf : invoice.getPerformances()) {
+            totalAmount += amountFor(perf, playFor(plays, perf));
+        }
+        return totalAmount;
     }
 
     private static int totalVolumeCredits(Invoice invoice, Map<String, Play> plays) {
